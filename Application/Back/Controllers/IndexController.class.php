07e2eb099b3b3288538398d0009741f847b93d0f
rsdawa <zyk.happy@gmail.com>
2017-08-31 10:46:46 +0800

    仿ecshop登录初步始现

--- Application/Back/Controllers/IndexController.class.php
+++ Application/Back/Controllers/IndexController.class.php
@@ -5,6 +5,23 @@ class IndexController extends BaseController
     {
         $M    = ModelFactory::M("IndexModel");
         $data = $M->GetHello();
-        include VIEW_PATH . 'index.html';
+        include VIEW_PATH . 'login.html';
+    }
+
+    public function CheckLogin()
+    {
+        //1.获取表单信息（用户名、密码）
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        //2.调用模型进行验证
+        $model  = ModelFactory::M("IndexModel");
+        $result = $model->CheckLogin($user, $pass);
+        //3.返回结果
+        if ($result) {
+            echo '<h1>欢迎进入管理后台</h1>';
+        } else {
+            $this->MsgAndGo('用户名和密码不匹配', '?m=Back&c=Index');
+        }
     }
 }
--- Application/Back/Models/IndexModel.class.php
+++ Application/Back/Models/IndexModel.class.php
@@ -5,4 +5,16 @@ class IndexModel extends BaseModel
     {
         return "欢迎来到后台";
     }
+
+    public function CheckLogin($user, $pass)
+    {
+        $sql    = "select count(*) as c from user_list where user_name='$user' and user_pass='$pass'";
+        $result = $this->db->getOneData($sql);
+        if ($result == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
